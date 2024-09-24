325125546bc516ea12cf950cd335111ad8f3b4da
Muntakim61 <mostafa.muntakim61@gmail.com>
2024-09-24 17:34:08 +0600

    blog page started

--- js/donate.js
+++ js/donate.js
@@ -17,10 +17,19 @@ function calculateNewDonationAmount(a,b)
     const c=a+b
     return c
 }
+function containsLetter(S) {
+    const letterPattern=/[a-zA-Z]/;
+    return letterPattern.test(S);
+}
 function getDonatedAmountByID(id){
     const donateText = document.getElementById(id).value
     if(donateText == '')
     {
+        alert('Invalid Input.Please try a different input')
+        return 0
+    }
+
+    if(containsLetter(donateText)){
         alert('Invalid Input.Please try a different input')
         return 0
     }
@@ -45,15 +54,20 @@ document.getElementById('btn-donate-noakhali')
     const mA = getMainBalance()
     if(donateAmount<0){
         alert('Invalid Input.Please try a different input')
+        updateDonatedAmountTextField('input-donate-noakhali')
     }
     else if(donateAmount > mA){
         alert('Insufficient Balance.Please top up')
+        updateDonatedAmountTextField('input-donate-noakhali')
     }
     else{
         const result = calculateNewDonationAmount(donateAmount,noakhaliBalanceNumber)
         document.getElementById('noakhali-donate-account').innerText = result
         updateMainBalance(donateAmount)
         updateDonatedAmountTextField('input-donate-noakhali')
+        if(donateAmount!=0){
+            writeHistory('noakhali-title',donateAmount)
+        }
     } 
 })
 // Feni donate JS section
@@ -65,15 +79,21 @@ document.getElementById('btn-donate-feni')
     const mA = getMainBalance()
     if(donateAmount<0){
         alert('Invalid Input.Please try a different input')
+        updateDonatedAmountTextField('input-donate-feni')
     }
     else if(donateAmount > mA){
         alert('Insufficient Balance.Please top up')
+        updateDonatedAmountTextField('input-donate-feni')
     }
     else{
         const result = calculateNewDonationAmount(donateAmount,feniBalanceNumber)
         document.getElementById('feni-donate-account').innerText = result
         updateMainBalance(donateAmount)
         updateDonatedAmountTextField('input-donate-feni')
+        if(donateAmount!=0){
+            writeHistory('feni-title',donateAmount)
+        }
+            
     } 
 })
 // Qouta Protest donate JS section
@@ -85,14 +105,19 @@ document.getElementById('btn-donate-qouta')
     const mA = getMainBalance()
     if(donateAmount<0){
         alert('Invalid Input.Please try a different input')
+        updateDonatedAmountTextField('input-donate-quota')
     }
     else if(donateAmount > mA){
         alert('Insufficient Balance.Please top up')
+        updateDonatedAmountTextField('input-donate-quota')
     }
     else{
         const result = calculateNewDonationAmount(donateAmount,quotaBalanceNumber)
         document.getElementById('quota-donate-account').innerText = result
         updateMainBalance(donateAmount)
         updateDonatedAmountTextField('input-donate-quota')
+        if(donateAmount!=0){
+            writeHistory('quota-title',donateAmount)
+        }
     } 
 })
--- js/history.js
+++ js/history.js
@@ -3,21 +3,25 @@
 //     // console.log('History was clicked')
     
 // })
-function writeHistory(id)
+function writeHistory(id , amount)
 {
     const divName = document.createElement('div');
     const paragraph1 = document.createElement('p');
     const paragraph2 = document.createElement('p');
-    divName.className = 'hero border rounded-xl w-[70%] display: block mx-auto p-8 shadow-lg';
-    divName.innerHTML = '<h1>Hello, I am a Hero div!</h1>';
+    divName.className = 'hero border rounded-xl w-[70%] display: block mx-auto p-8 shadow-lg mb-8';
     paragraph1.className = 'font-bold mb-4 text-xl'
-    const parent=document.getElementById('transaction-history-content');
-    if (parent) {
-         parent.appendChild(divName);
-    }else
-    {
-        console.log('The div with id="transaction-history-content" was not found.');
-    }
+    const ptext = document.getElementById(id).innerText
+    console.log(ptext)
+    // paragraph1.value = ('Donated Tk ' + amount + 'to ' + ptext)
+    console.log('Donated Tk ' + amount + ' to ' + ptext)
+    paragraph1.innerText = ('Donated Tk ' + amount + ' to ' + ptext)
+    paragraph2.innerText = ('Date : ' + new Date())
+    console.log(paragraph1.value)
+
+    divName.appendChild(paragraph1)
+    divName.appendChild(paragraph2)
+    const parent = document.getElementById('transaction-history-content');
+    parent.appendChild(divName)
 
 
 } 
